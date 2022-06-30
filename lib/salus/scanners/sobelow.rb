a7f561ca028d451468fd38b5b2c5f76569e36c1c
alex0112 <alarsen0112@gmail.com>
2022-06-30 11:47:15 -0600

    added sobelow scanner

--- lib/salus/scanners/sobelow.rb
+++ lib/salus/scanners/sobelow.rb
@@ -5,6 +5,8 @@ require 'json'
 module Salus::Scanners
   class Sobelow < Base
 
+    include Salus::Formatting
+
     def version
       return '0.11.1' ## TODO: Currently sobelow does not support a --version flag, get this info somehow
     end
@@ -15,17 +17,30 @@ module Salus::Scanners
 
     def run
       Dir.chdir(@repository.path_to_repo) do
-        shell_return = run_shell("mix sobelow -i Config.HTTPS --threshold medium --skip --private")
-        
-        if shell_return.success?
-          report_success
-        else
-          report_error(shell_return.stderr)
+        begin
+          shell_return = run_shell("mix sobelow -i Config.HTTPS --threshold medium --skip --private --exit medium")
+          handle_shell_output(shell_return)
+        rescue
+          report_error(shell_return.stderr + shell_return.stdout)
         end
 
       end
 
     end
 
+    def supported_languages
+    end
+
+    private
+    def handle_shell_output(shell_return)
+      if shell_return.success?
+        report_success
+      else
+        report_failure ## Mark this scan as failed
+        report_stderr(shell_return.stderr)
+        report_stdout(shell_return.stdout)
+        log(shell_return.stderr + shell_return.stdout)
+      end
+    end
   end
 end
