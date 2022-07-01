4523d5c26ab6662e642935af9948227d31026823
alex0112 <alarsen0112@gmail.com>
2022-06-30 22:43:02 -0600

    added scanner_type to meet spec

--- lib/salus/scanners/sobelow.rb
+++ lib/salus/scanners/sobelow.rb
@@ -7,37 +7,9 @@ module Salus::Scanners
 
     include Salus::Formatting
 
-    #     Sobelow is a static analysis tool for discovering vulnerabilities in Phoenix
-    # applications.
-
-    # This tool should be run in the root of the project directory with the following
-    # command:
-
-    #     mix sobelow
-
-    # ## Command line options
-
-    #   - --root -r - Specify application root directory
-    #   - --verbose -v - Print vulnerable code snippets
-    #   - --ignore -i - Ignore modules
-    #   - --ignore-files - Ignore files
-    #   - --details -d - Get module details
-    #   - --all-details - Get all module details
-    #   - --private - Skip update checks
-    #   - --strict - Exit when bad syntax is encountered
-    #   - --mark-skip-all - Mark all printed findings as skippable
-    #   - --clear-skip - Clear configuration added by --mark-skip-all
-    #   - --skip - Skip functions flagged with #sobelow_skip or tagged with
-    #     --mark-skip-all
-    #   - --router - Specify router location
-    #   - --exit - Return non-zero exit status
-    #   - --threshold - Only return findings at or above a given confidence level
-    #   - --format - Specify findings output format
-    #   - --quiet - Return no output if there are no findings
-    #   - --compact - Minimal, single-line findings
-    #   - --save-config - Generates a configuration file based on command line
-    #     options
-    #   - --config - Run Sobelow with configuration file
+    def self.scanner_type
+      Salus::ScannerTypes::SAST
+    end
 
     def version
       return '0.11.1' ## TODO: Currently sobelow does not support a --version flag, get this info somehow
@@ -48,6 +20,38 @@ module Salus::Scanners
     end
 
     def run
+      #     Sobelow is a static analysis tool for discovering vulnerabilities in Phoenix
+      # applications.
+      
+      # This tool should be run in the root of the project directory with the following
+      # command:
+
+      #     mix sobelow
+
+      # ## Command line options
+
+      #   - --root -r - Specify application root directory
+      #   - --verbose -v - Print vulnerable code snippets
+      #   - --ignore -i - Ignore modules
+      #   - --ignore-files - Ignore files
+      #   - --details -d - Get module details
+      #   - --all-details - Get all module details
+      #   - --private - Skip update checks
+      #   - --strict - Exit when bad syntax is encountered
+      #   - --mark-skip-all - Mark all printed findings as skippable
+      #   - --clear-skip - Clear configuration added by --mark-skip-all
+      #   - --skip - Skip functions flagged with #sobelow_skip or tagged with
+      #     --mark-skip-all
+      #   - --router - Specify router location
+      #   - --exit - Return non-zero exit status
+      #   - --threshold - Only return findings at or above a given confidence level
+      #   - --format - Specify findings output format
+      #   - --quiet - Return no output if there are no findings
+      #   - --compact - Minimal, single-line findings
+      #   - --save-config - Generates a configuration file based on command line
+      #     options
+      #   - --config - Run Sobelow with configuration file
+
       shell_return = run_shell(command, chdir: @repository.path_to_repo)
       handle_shell_output(shell_return)
     end
@@ -72,7 +76,7 @@ module Salus::Scanners
         report_success
       else
         report_failure ## Mark this scan as failed
-        report_stderr(shell_return.stderr) ## If we end up here it's fine to fail the scan
+        report_stderr(shell_return.stderr)
         report_stdout(shell_return.stdout)
 
         log(shell_return.stderr + shell_return.stdout) ## In the event of failure, log the output in its entirety
